c9fcd41ebb64cc938cb25ec2caa4c977fdf57d47
JasonBeagle <skipskipperson385@gmail.com>
2023-03-31 11:39:38 -0700

    task0,correction

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,46 +1,73 @@
 #!/usr/bin/python3
 
-"""Python script that, using this REST API, for a given employee ID"""
+"""
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 
 import requests
 import sys
 
+USERS_API = "https://jsonplaceholder.typicode.com/users/"
+TODOS_API = "https://jsonplaceholder.typicode.com/todos"
+
+
+def get_employee_todos(employee_id):
+    """
+    Given an employee ID, returns information about their TODO list progress.
+    """
+
+    # Fetch the employee's details from the API.
+    employee_api_url = USERS_API + str(employee_id)
+    employee_response = requests.get(employee_api_url)
+    employee = employee_response.json()
+
+    # Fetch the employee's TODO list from the API.
+    todos_api_url = TODOS_API + "?userId=" + str(employee_id)
+    todos_response = requests.get(todos_api_url)
+    todos = todos_response.json()
+
+    # Count the number of completed and total tasks.
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos)
+
+    # Construct the output string.
+    output = "Employee {} is done with tasks({}/{}):\n".format(
+        employee['name'], num_completed_tasks, total_tasks)
+
+    # Add the title of completed tasks to the output string.
+    for task in completed_tasks[:-1]:
+        output += "\t" + task['title'] + '\n'
+
+    output += "\t" + completed_tasks[-1]['title']
+
+    return output
+
+
+def main():
+    # Check if the employee ID was provided as a command-line argument.
+    if len(sys.argv) != 2:
+        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Parse the employee ID.
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Invalid employee ID.")
+        sys.exit(1)
+
+    # Get the employee's TODO list progress.
+    try:
+        output = get_employee_todos(employee_id)
+    except requests.exceptions.RequestException as e:
+        print("Error: Could not fetch employee data from API.")
+        sys.exit(1)
+
+    # Print the output.
+    print(output)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        """Get the user ID from command line argument"""
-        user_id = sys.argv[1]
-
-        """Construct the API URL using the user ID"""
-        url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-
-        """Send a GET request to the API URL"""
-        response = requests.get(url)
-
-        """Check if the response is successful"""
-        if response.status_code == 200:
-            """Get the response data in JSON format"""
-            data = response.json()
-
-            """Get the name of the user from the API"""
-            user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-            user_response = requests.get(user_url)
-            user_data = user_response.json()
-            user_name = user_data["name"]
-
-            """Calculate the number of completed tasks and total tasks"""
-            num_completed_tasks = 0
-            total_tasks = len(data)
-            for task in data:
-                if task["completed"]:
-                    num_completed_tasks += 1
-
-            """Print the result"""
-            print(f"Employee {user_name} is done with tasks\
-                ({num_completed_tasks}/{total_tasks}):")
-            for task in data:
-                if task["completed"]:
-                    print(f"\t {task['title']}")
-        else:
-            print(f"Error: {response.status_code}")
-    else:
-        print("Please provide a user ID as a command line argument.")
+    main()
