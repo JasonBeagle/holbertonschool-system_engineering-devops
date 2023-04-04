b6b21a1fc97cad3cc8a3401db388066c30c0252a
JasonBeagle <skipskipperson385@gmail.com>
2023-04-04 20:31:28 +0000

    task3 pycode corrections

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -18,20 +18,23 @@ if __name__ == "__main__":
     users = requests.get('https://jsonplaceholder.typicode.com/users',
                          verify=False).json()
     todo = requests.get('https://jsonplaceholder.typicode.com/todos',
-                         verify=False).json()
+                        verify=False).json()
 
     # Initialize dictionaries to store user and task data
     user_dict = {}
     usernamedict = {}
 
-    # Iterate through the retrieved user data and create a key-value pair for each user ID
-    # in the user dictionary, and store the username for each user ID in the usernamedict
+    # Iterate through the retrieved user data and create a key-value pair
+    # for each user ID
+    # in the user dictionary, and store the username for each user ID
+    # in the usernamedict
     for user in users:
         ID = user.get("id")
         user_dict[ID] = []
         usernamedict[ID] = user.get('username')
 
-    # Iterate through the retrieved task data and add task data to the user dictionary
+    # Iterate through the retrieved task data
+    # and add task data to the user dictionary
     for task in todo:
         task_dict = {}
         ID = task.get("userId")
