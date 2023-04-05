9597c3863ef658c07e496e202fdacb1b56165ed9
JasonBeagle <skipskipperson385@gmail.com>
2023-04-05 15:02:31 +0000

    task3,correction attempt 345678234

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
-
-"""
-This script extends the previous version to export data in
-JSON format for all employees' progress on their TODO lists,
-based on data from the JSONPlaceholder API.
-"""
-
+"""Retrieves TODO list progress of all employees from a REST API and
+exports data to a JSON file."""
 import json
 import requests
-from sys import argv
 
-# Check if this module is being executed as the main program
-if __name__ == "__main__":
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
 
-    # Make requests to the user and TODO list APIs to retrieve data
-    # Set verify to False to ignore SSL certificate verification
-    users = requests.get('https://jsonplaceholder.typicode.com/users',
-                         verify=False).json()
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
-                        verify=False).json()
+    if response.status_code != 200:
+        print("Error: Request failed with status code {}".format(
+            response.status_code))
+        exit(1)
 
-    # Initialize dictionaries to store user and task data
-    user_dict = {}
-    usernamedict = {}
+    todos = response.json()
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    response_users = requests.get(users_url)
 
-    # Iterate through the retrieved user data and create a key-value pair
-    # for each user ID
-    # in the user dictionary, and store the username for each user ID
-    # in the usernamedict
+    if response_users.status_code != 200:
+        print("Error: Request failed with status code {}".format(
+            response_users.status_code))
+        exit(1)
+
+    users = response_users.json()
+    todo_all_employees = {}
+
     for user in users:
-        ID = user.get("id")
-        user_dict[ID] = []
-        usernamedict[ID] = user.get('username')
+        user_id = user['id']
+        employee_name = user['username']
+        employee_todos = []
 
-    # Iterate through the retrieved task data
-    # and add task data to the user dictionary
-    for task in todo:
-        task_dict = {}
-        ID = task.get("userId")
-        task_dict['username'] = usernamedict.get(ID)
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        user_dict.get(ID).append(task_dict)
+        for todo in todos:
+            if todo['userId'] == user_id:
+                employee_todos.append({
+                    'task': todo['title'],
+                    'completed': todo['completed'],
+                    'username': employee_name
+                })
 
-    # Write the user dictionary to a JSON file
-    with open("todo_all_employees.json", 'w') as jsfile:
-        json.dump(user_dict, jsfile)
+        todo_all_employees[user_id] = employee_todos
+
+    with open("todo_all_employees.json", mode="w") as outfile:
+        json.dump(todo_all_employees, outfile)
